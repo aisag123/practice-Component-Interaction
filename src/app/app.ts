141a8e98f40638dcdaa793b19602c9a8a5d62533
aiden123 <aidensagaser@gmail.com>
2026-03-16 10:49:54 -0500

    app

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,16 +7,25 @@ import { Child } from './child/child';
   template: `
     <div class="container parent">
       <h2>Parent Component (App Component)</h2>
-      <p>Message from Child: </p>
+      <!-- message to child: <input type="text" [(ngModel)] = "parentMessage"></input> -->
+      <p>Message from Child: {{childMessage()}}</p> 
       <div>
-        <app-child></app-child>
+        <app-child
+        (clicked) = "receiveMessage($event)"
+        />
       </div>
     </div>
   `,
   styles: `.parent{background-color: pink; color: black}`
 })
 export class App {
+  parentMessage = signal<string>('');
+  childMessage = signal<string>('');
 
+  receiveMessage(msg: string) {
+    this.childMessage.set(msg); //this sets child to msg
+
+  }
 }
 
 
